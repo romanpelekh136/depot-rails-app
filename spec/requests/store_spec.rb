ad5d8b48bcdd47a4950d2b8753a152dd55f04ac4
romanpelekh136 <rpeleh19@gmail.com>
2026-01-21 00:46:15 +0000

    style and test store page

--- spec/requests/store_spec.rb
+++ spec/requests/store_spec.rb
@@ -3,9 +3,15 @@ require 'rails_helper'
 RSpec.describe "Stores", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      get "/store/index"
+      create(:product)
+
+      get store_index_path
       expect(response).to have_http_status(:success)
+
+      expect(response.body).to include("Home")
+      expect(response.body).to include("Contact")
+
+      expect(response.body).to match(/\$[,\d]+\.\d\d/)
     end
   end
-
 end
